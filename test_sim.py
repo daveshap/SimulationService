6a26a59ff2b2e13b646a6799e597eb56b4052447
David Shapiro <daveshap37@gmail.com>
2022-07-27 07:07:12 -0400

    done for now

--- test_sim.py
+++ test_sim.py
@@ -18,7 +18,7 @@ openai.api_key = open_file('openaiapikey.txt')
 ws_dir = 'worldstates/'
 
 
-def gpt3_completion(prompt, engine='text-davinci-002', temp=0.5, top_p=1.0, tokens=1000, freq_pen=0.0, pres_pen=0.0, stop=['asdfasdf', 'asdasdf']):
+def gpt3_completion(prompt, engine='text-davinci-002', temp=1.0, top_p=1.0, tokens=1000, freq_pen=0.0, pres_pen=0.0, stop=['asdfasdf', 'asdasdf']):
     max_retry = 5
     retry = 0
     prompt = prompt.encode(encoding='ASCII',errors='ignore').decode()
